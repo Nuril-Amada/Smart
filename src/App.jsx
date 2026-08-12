b2c8e18ff0ae329e91e595d06999be116db658df
Nuril-Amada <amadanuril@gmail.com>
2026-08-12 14:18:49 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Dashboard";
 import CetakCek from "./pages/CetakCek";
 import Settlement from "./pages/Settlement";
 import Advance from "./pages/Advance";
+import CashOpname from "./pages/CashOpname";
 import MasterData from "./pages/MasterData";
 
 function App() {
@@ -25,6 +26,9 @@ function App() {
 
         {/* Advance */}
         <Route path="/Advance" element={<MainLayout><Advance /></MainLayout>} />
+
+        {/* Cash Opname */}
+        <Route path="/CashOpname" element={<MainLayout><CashOpname /></MainLayout>} />
 
         {/* Master Data */}
         <Route path="/MasterData" element={<MainLayout><MasterData /></MainLayout>} />
